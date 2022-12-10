e282d1d2a299b53cde16dc98c1328f8e2a173953
blintgen <111295995+blintgen@users.noreply.github.com>
2022-12-09 18:23:45 -0600

    Add files via upload

---
+++ PicklingBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PicklingBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+        }
+    }
+}
