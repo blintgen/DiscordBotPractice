9cb39443a27eaa03a1e048de8a41ca36d05792eb
Elijah Kulesa <elijahkulesa@gmail.com>
2022-12-23 16:55:47 -0600

    Added template code

---
+++ DiscordBotPractice/Repositories/RepoFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardPicklerBot.Repositories
+{
+    internal class RepoFactory
+    {
+        public static IUserRepo GetUserRepo() => throw new NotImplementedException(); //todo
+        public static IDeckRepo GetDeckRepo() => throw new NotImplementedException(); //todo
+    }
+}
---
+++ DiscordBotPractice/Repositories/Runtime/UserRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardPicklerBot.Repositories.Runtime
+{
+    internal class UserRepo
+    {
+    }
+}
